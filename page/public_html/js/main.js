49d111b49b0d5dd087d13193083deba3acf6e5f9
tanna <tanna@192.168.0.108>
2018-06-08 11:42:22 +0800

    添加左右布局功能及菜单隐藏显示

--- page/public_html/js/main.js
+++ page/public_html/js/main.js
@@ -1,29 +1,21 @@
 
-$(".nva-btn").on("click",function(){
-    var $width=$("#admin_left").width();
-    if($width>50){
-        // 收起操作
-        //alert("收起操作");
+$(".nva-btn").on("click", function () {
+    var $width = $("#admin_left").width();
+    if ($width > 50) {// 收起操作
         admin_left_collapse();
-    }else{
-       // 展开操作
-      // alert("展开操作");
-      admin_left_expand();
+    } else {// 展开操作
+        admin_left_expand();
     }
 });
 
 // 收起操作
-function admin_left_collapse(){
-   // $("#admin_left").css({'min-width':'50px'});
-   // $("#admin_main").css({'margin-left':'50px'});
-           $("#admin_left").animate({'min-width':'50px'},500);
-           $("#admin_main").animate({'margin-left':'50px'},500);
+function admin_left_collapse() {
+    $("#admin_left").animate({'min-width': '50px'}, 500);
+    $("#admin_main").animate({'margin-left': '50px'}, 500);
 }
 
 // 展开操作
-function admin_left_expand(){
-  //  $("#admin_left").css({'min-width':'220px'});
-  //  $("#admin_main").css({'margin-left':'220px'});
-     $("#admin_left").animate({'min-width':'220px'},500);
-           $("#admin_main").animate({'margin-left':'220px'},500);
+function admin_left_expand() {
+    $("#admin_left").animate({'min-width': '220px'}, 500);
+    $("#admin_main").animate({'margin-left': '220px'}, 500);
 }
