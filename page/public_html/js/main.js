0336bab973fe5f0dc10803b55d7362539884ee4f
wsh <wsh@linux>
2018-06-09 21:54:16 +0800

    update

--- page/public_html/js/main.js
+++ page/public_html/js/main.js
@@ -1,3 +1,10 @@
+$(function(){
+   reset_layout(); 
+});
+
+$(window).resize(function () {
+    reset_layout();
+});
 
 $(".nva-btn").on("click", function () {
     var $width = $("#admin_left").width();
@@ -10,12 +17,25 @@ $(".nva-btn").on("click", function () {
 
 // 收起操作
 function admin_left_collapse() {
+    
     $("#admin_left").animate({'min-width': '50px'}, 500);
     $("#admin_main").animate({'margin-left': '50px'}, 500);
+    $(".brand_title").hide(1000);
+    $(".user-panel").hide(1000);
+
 }
 
 // 展开操作
 function admin_left_expand() {
+    
     $("#admin_left").animate({'min-width': '220px'}, 500);
     $("#admin_main").animate({'margin-left': '220px'}, 500);
+    $(".brand_title").show(1000);
+    $(".user-panel").show(1000);
+}
+
+function reset_layout() {
+    if ($(this).width() < 767) {
+        admin_left_collapse();
+    }
 }
